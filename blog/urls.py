bdbc34ed97d3a1671a95e105c3555a5ad451b6e7
Sumit Karmacharya <sumitkarmacharya@gmail.com>
2022-03-14 09:13:09 +0545

    blog project added

--- blog/urls.py
+++ blog/urls.py
@@ -16,10 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-from my_app.views import home
+from my_app.views import home, about
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 
     path('',home, name='home'),
+    path('about', about,name='about'),
 ]
--- my_app/views.py
+++ my_app/views.py
@@ -1,4 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib.auth.models import User
+
 def home(request):
-    return HttpResponse("This is my home")
+    if request.user.is_authenticated:
+        email=request.user.email
+    else:
+        email=''
+    return render(request,'index.html',{'name':'Sumit','email':email})
+
+def about(request):
+    return HttpResponse("This is about seciton")
